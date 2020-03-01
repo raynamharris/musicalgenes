13fc6027445989c944689476cca6fa735c1f1743
raynamharris <rayna.harris@utexas.edu>
2020-03-01 08:21:11 -0800

    now with spanish prototype

---
+++ es/ui.R
@@ -0,0 +1,37 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+gene_names <- df %>% distinct(gene) %>% pull()
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Expresión génica en palomas parentales"),
+ 
+  # Sidebar with a slider input for boxplot
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "variable",
+                  label = "gen candidato",
+                  choices = c(gene_names),
+                  selected = "PRL",
+                  multiple = FALSE)
+    ),
+    
+    # boxplot
+    mainPanel(
+       plotOutput("boxPlot", height = "750px")
+    )
+  )
+))
+
+
--- plotNplay.R
+++ plotNplay.R
@@ -1,52 +1,42 @@
-library(tidyverse)
-library(sonify)
 
-### setup themes
-source("themes.R")
-
-### get data
-
-df <- read_csv("./candidatecounts.csv")
-df$treatment <- factor(df$treatment, levels = charlevels)
-df$tissue <- factor(df$tissue, levels = tissuelevels)
-
-## plot data
-
-candidategenes <- calisigenes <- c("PRL", "PRLR", "AVPR1B", "MYC",
-                                   "MAP2KA",   "MSH", "PALB2", "BRCA1")
-
-plotcandidatecounts <- function(df, whichtstages, whichsex, whichtissue){
-  
-  myylab = paste(whichtissue, "gene expression" , sep = "")
+plotcandidatecounts <- function(df, whichtstages, 
+                                whichsex, whichtissue){
+  myylab = paste(whichsex, whichtissue, "gene expression" , sep = " ")
   
   p <- df %>%
     dplyr::filter(treatment %in% whichtstages,
                   tissue %in% whichtissue,
                   sex %in% whichsex) %>%
-    ggplot(aes(x = treatment, y = counts, fill = treatment, color = sex)) +
-    geom_boxplot() + facet_wrap(~gene, scales = "free_y") + 
+    ggplot(aes(x = treatment, y = counts, color = sex)) +
+    geom_boxplot(aes(fill = treatment)) + 
+    geom_smooth(aes(x = as.numeric(treatment)), se = F) +
+    facet_wrap(~gene, scales = "free_y") + 
     theme_classic() +
-    scale_fill_manual(values = allcolors) +
+    scale_fill_manual(values = allcolors, guide=FALSE) +
     scale_color_manual(values = allcolors) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1),
-          legend.position = "none",
+          legend.position = "bottom",
           #axis.title.x = element_blank(),
           strip.text = element_text(face = "italic")) +
     labs(y = myylab, x = "Parental stages") 
   print(p)
 }
 
-plotcandidatecounts(df, charlevels, "female", "pituitary") 
-plotcandidatecounts(df, charlevels, "male", "pituitary") 
+df %>% filter(gene %in% c("PRL")) %>%
+  plotcandidatecounts(charlevels, "female", "pituitary") 
+
+df %>% filter(gene %in% c("BRCA1" , "PRL", "MYC")) %>%
+  plotcandidatecounts(charlevels,  "female", "pituitary") 
 
 ### play sounds
 
 sonifycandidatecounts <- function(df, whichgene, whichtissue, whichsex){
   
   medianvalues <- df %>%
-    dplyr::filter(gene %in% whichgene,
+    dplyr::filter(gene %in% input$variable,
                   tissue %in% whichtissue,
                   sex %in% whichsex) %>%
+    filter(treatment != "NA" ) %>%
     group_by(treatment, sex, tissue) %>%
     summarize(median = median(counts, na.rm = TRUE),
               sdev = sd(counts, na.rm = TRUE)) %>%
@@ -59,16 +49,12 @@ sonifycandidatecounts <- function(df, whichgene, whichtissue, whichsex){
   return(sonification)
 }
 
-sonifycandidatecounts(df, "AVPR1B","pituitary", "female")
 sonifycandidatecounts(df, "BRCA1","pituitary", "female")
 sonifycandidatecounts(df, "MYC","pituitary", "female")
-sonifycandidatecounts(df, "PALB2","pituitary", "female")
 sonifycandidatecounts(df, "PRL","pituitary", "female")
-sonifycandidatecounts(df, "PRLR","pituitary", "female")
 
-sonifycandidatecounts(df, "AVPR1B","pituitary", "male")
-sonifycandidatecounts(df, "BRCA1","pituitary", "male")
-sonifycandidatecounts(df, "MYC","pituitary", "male")
-sonifycandidatecounts(df, "PALB2","pituitary", "male")
-sonifycandidatecounts(df, "PRL","pituitary", "male")
-sonifycandidatecounts(df, "PRLR","pituitary", "male")
+sonifycandidatecounts(df, "BRCA1","hypothalamus", "male")
+sonifycandidatecounts(df, "MYC","hypothalamus", "male")
+sonifycandidatecounts(df, "PRL","hypothalamus", "male")
+
+
