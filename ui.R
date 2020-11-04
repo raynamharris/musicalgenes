d5ffd0cb14dcc4ee6fa60d2be5b0704d3965aff4
raynamharris <rayna.harris@utexas.edu>
2020-11-04 12:51:55 -0800

    now with tabs

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ shinyUI(
   fluidPage(
     
     # Application title
-    titlePanel("Musical Genes: Visualize and sonify gene expression in parenting pigeons"),
+    titlePanel("Musical Genes: Visualize and sonify gene expression"),
 
     # titlePanel(title=div(img(src="expdesign.png"))),
   
@@ -16,21 +16,18 @@ shinyUI(
         
         wellPanel(
           
-          h4("Transcriptional symphony"), 
+          h4("Molecular Symphony"), 
           
           p("Temporally controlled changes in gene expression 
-            are often described as a “transcriptional symphony.” 
-              What does these transcriptional symphony of parental 
-              care in parenting pigeons sound like?"),
-          
-            p("Data sonification is the presentation of data as sound. 
+            are often described as a symphony that is regulated by transcription factors. 
+            What does this symphony of coordinated changes (or cacophony of 
+            discoordinated changes) in gene expression sound like? 
+            Data sonification is the presentation of data as sound. 
             Here, you can interactively visualize and sonify gene expression 
             to better understand the molecular mechanisms that regulate
             parental care behavior and other important phenotypes."),
           
-          p("After choosing a gene, tissue and sex, click the 'sonify' 
-            button to listen the mean value of gene expression."),
-            
+
           selectInput(
             inputId = "gene",
             label = "Choose a gene.",
@@ -57,19 +54,8 @@ shinyUI(
             multiple = FALSE
           ),
           
-          tableOutput("genedescrip"),
-          
-          tableOutput("goterms"),
-          
-          tableOutput("genedisease"),
-          
-          p("Descriptions, diseases, and biological processes associated with the human ortholog of the gene of interest are provided by the ",
-            tags$a(
-              href = "https://www.alliancegenome.org/downloads#gene-descriptions",
-              "Alliance of Genome Resources.")
-          ),
-          
-          
+         p("After choosing a gene, tissue and sex, click the 'sonify' 
+            button to listen the mean value of gene expression."),
           
          p("Don't see your favorite gene?", 
            
@@ -77,14 +63,8 @@ shinyUI(
              href = "https://github.com/raynamharris/musicalgenes/issues/new?assignees=&labels=&template=request-a-gene.md&title=Gene+Request",
              "Check here to create a request."
            )
-         )
+         ),
         
-        ),
-        
-        wellPanel(
-          
-          HTML(paste(h4("Open source software for reproducible research"))),
-          
           
           p("The code for this webpage is", 
             
@@ -101,10 +81,42 @@ shinyUI(
             tags$a(
               href = "https://macmanes-lab.github.io/DoveParentsRNAseq/",
               "@macmanes-lab/DoveParentsRNAseq"
-              
             )
-          )
-        )
+          ),
+         
+         h4("Learn more"),       
+         
+         p("Before building this app, I used a keyboard to play 
+           the sound of genes working together to regulate parental care. 
+           You can watch these videos to learn more about the inspiration behind this app."),
+         
+         
+         HTML('<iframe width="95%" height = "200px" 
+              src="https://www.youtube.com/embed/PoKiIwIsLSo" 
+              frameborder="0" allow="accelerometer; autoplay; 
+              encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+         
+        ),
+        
+
+        HTML('<iframe width="95%" height="200px" 
+             src="https://www.youtube.com/embed/ssGuxnD_NCo" 
+             frameborder="0" allow="accelerometer; autoplay; 
+             encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'),
+        
+        
+        h4("Credit"), 
+        
+        p("This app is a product of the 'Birds, Brains, and Banter (B3)'
+          Laboratory at the University of California at Davis.
+          This and related research is funded by the National Science Foundation,
+          in a grant to Rebecca Calisi and Matthew MacManes.
+          The webpage was created and written by Rayna Harris and Rebecca Calisi.  
+          Owen Marshall, Mauricio Vargas, Titus Brown, and 
+          Alexandra Colón Rodríguez contributed intellectually to the design. 
+          Suzanne Austin, Andrew Lang, Victoria Farrar, April Booth, 
+          Tanner Feustel, and Matthew MacManes contributed the 
+          experiment that is the foundation of this application.")
       
         
       ),
@@ -112,10 +124,12 @@ shinyUI(
       ### main panel
       mainPanel(
         
-        wellPanel(
+        tabsetPanel(
+          tabPanel("Transcriptional Symphony", 
+                   
+                   h4("Transcriptional symphony"),      
               
-          h4("Listen to the sound of gene expression changing over time"),      
-          
+
           HTML('<center><img src="fig_musicalgenes.png", width = "100%"></center>'),
               
           
@@ -128,7 +142,7 @@ shinyUI(
           
           actionButton("button", "Sonify the average gene expression value."),
           
-          downloadButton("wav_dln", label = "Download to play."),
+          downloadButton("wav_dln", label = "Download wav file."),
           
           uiOutput("audiotag"),
           
@@ -149,69 +163,49 @@ shinyUI(
         
         ),
         
+        tabPanel("Hormonal symphony" ,
+                 
+                 h4("Hormonal symphony"),      
+                 
+                 p("There is an intricate interplay between genes and hormones.
+                   Genes directly or indirectly encode hormones and the receptors they bind to,
+                   and hormones can module to the expression levels of many genes.
+                   Here, we show the correlation pattern between the gene of interest and four hormones
+                   (prolactin (prl), corticosterone (cort), progesterone (p4), 
+                   and estradiol (e2) in females or testosterone (t) in males." ),
+                 
+                 plotOutput("hormoneplots")
+                 ),
         
-        wellPanel(  
-          
-          h4("Genes and hormones"), 
-          
-          p("There is an intricate interplay between genes and hormones.
-            Genes directly or indirectly encode hormones and the receptors they bind to,
-            and hormones can module to the expression levels of many genes.
-            Here, we show the correlation pattern between four hormones
-            (prolactin (prl), corticosterone (cort), progesterone (p4), 
-            and estradiol (e2) in females or testosterone (t) in males." ),
-          
-          plotOutput("hormoneplots")
-          
-        ),
+        tabPanel("Gene Information",
+                 
+                 h4("Gene information"),  
+                
+                 p("What biological processes and diseases are associated with the gene of interest?
+                    The following descriptions, diseases, and biological processes associated with the selected 
+                   gene of interest are provided by the ",
+                   tags$a(
+                     href = "https://www.alliancegenome.org/downloads#gene-descriptions",
+                     "Alliance of Genome Resources.")
+                 ),
+                   
+                 tableOutput("genedescrip"),
+                 
+                 tableOutput("goterms"),
+                 
+                 tableOutput("genedisease")
+                 ),
         
-        
-        wellPanel(  
-          
-          h4("Future directions"), 
-          
-          p("In the future, we would like to be able sonify data for multiple genes or hormones simultaneously using sounds from instruments found in an orchestra." ),
-          
-          tableOutput("orchestratable")
-          
-          ),
-        
-        wellPanel(
-          h4("Learn more"),       
-             
-             p("Before building this app, I used a keyboard to play 
-               the sound of genes working together to regulate parental care. 
-               You can watch these videos to learn more about the inspiration behind this app."),
-          
-
-          HTML('<iframe width="47.5%" height = "200px" 
-                  src="https://www.youtube.com/embed/PoKiIwIsLSo" 
-               frameborder="0" allow="accelerometer; autoplay; 
-               encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'),
-          
-          
-          
-          
-         HTML('<iframe width="47.5%" height="200px" 
-               src="https://www.youtube.com/embed/ssGuxnD_NCo" 
-              frameborder="0" allow="accelerometer; autoplay; 
-               encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'),
-
-          
-          h4("Credit"), 
-          
-          p("This app is a product of the 'Birds, Brains, and Banter (B3)'
-            Laboratory at the University of California at Davis.
-            This and related research is funded by the National Science Foundation,
-            in a grant to Rebecca Calisi and Matthew MacManes.
-            The webpage was created and written by Rayna Harris and Rebecca Calisi.  
-            Owen Marshall, Mauricio Vargas, Titus Brown, and 
-            Alexandra Colón Rodríguez contributed intellectually to the design. 
-            Suzanne Austin, Andrew Lang, Victoria Farrar, April Booth, 
-            Tanner Feustel, and Matthew MacManes contributed the 
-            experiment that is the foundation of this application.")
-             )
+        tabPanel("Future Directions" ,
+                 
+                 h4("Future directions"),    
+                 
+                 p("In the future, we would like to be able sonify data for multiple genes or hormones simultaneously using sounds from instruments found in an orchestra." ),
+                 
+                 tableOutput("orchestratable")
+                   )
           )
+      )
           
           
     )
